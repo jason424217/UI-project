607ea90d3c9339886e7d4eeb93bdbf005caedd70
LDsBF <tw2774@columbia.edu>
2022-04-15 22:41:13 -0400

    initialize branch

--- application.py
+++ application.py
@@ -6,6 +6,18 @@ import numpy as np
 
 application = Flask(__name__)
 
+courses = {
+    "1": {
+        "position": "PG (Point Guard)",
+        "video": "https://www.youtube.com/watch?v=sjg1J-OimBA",
+        "courtPosition": "resources/PG_position.jpg",
+        "courtMoveMap": "resources/PG_movemap.jpg",
+        "description": "Brain of a team. Mainly responsible for dribbling ball through the court, organizing tactic and"
+                       " passing ball to teammate with space to shot. "
+                       "Famous player: Magic Johnson, Stephen Curry, Chris Paul"
+    }
+}
+
 # ROUTES
 @application.route('/')
 def homepage():
