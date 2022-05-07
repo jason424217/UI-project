d658e25a5f11d5c1d9905ea7212f815398e5bb9f
lilili77 <lsjohnli7@gmail.com>
2022-05-07 04:39:44 -0400

    fix: Learn pages css

--- static/learn.js
+++ static/learn.js
@@ -1,32 +1,31 @@
 function generateUI(data){
     $("#position_info").html(data.position)
-    $("#position_info").css({"margin-top":"10px","text-decoration":"underline"})
+    // $("#position_info").css({"margin-top":"10px","text-decoration":"underline"})
     if(id==6){
-        let card=$(`<div>`)
-        card.addClass("card")
-        let card_body=$(`<div>`)
-        card_body.addClass("card-body")
-        let img = $(`<div >`).html("")
-        img.addClass("col-md-8 text-center")
-        img.css({"margin":"10px"})
+        // let card=$(`<div>`)
+        // card.addClass("card")
+        // let card_body=$(`<div>`)
+        // card_body.addClass("card-body")
+        // let img = $(`<div >`).html("")
+        // img.addClass("col-md-8 text-center")
+        // img.css({"margin":"10px"})
+        //
+        // card_body.html(`<img style="width:100%" src=${data.courtImg}/>`)
+        // card.append(card_body)
+        // img.append(card)
+        // $("#image-row").append(img)
+        // card_body.append("<b>Court Movement</b>")
 
-        card_body.html(`<img style="width:100%" src=${data.courtImg}/>`)
-        card.append(card_body)
-        img.append(card)
-        $("#image-row").append(img)
-        card_body.append("<b>Court Movement</b>")
-
+        $("#allpos-img").attr("src",data.courtImg)
         $("#directQuiz").html("Lets take the quiz!");
     }
     else{
-    $("#position-img").attr("src",data.courtPosition)
-    $("#move-img").attr("src",data.courtMoveMap)
+      $("#position-img").attr("src",data.courtPosition)
+      $("#move-img").attr("src",data.courtMoveMap)
     }
+
     $("#source").attr("src",data.video)
-
-
     $("#position-text").html(data.description)
-
     // keep nav bar's seleced option
     $("#positionList").val(id);
 }
@@ -63,10 +62,10 @@ $(document).ready(()=>{
         $("#prev").attr('disabled',true)
     }else if(id==6){
         $("#next").attr('disabled',true)
-        const row= document.getElementById("image-row")
-        while(row.hasChildNodes()){
-            row.removeChild(row.firstChild)
-        }
+        // const row= document.getElementById("image-row")
+        // while(row.hasChildNodes()){
+        //     row.removeChild(row.firstChild)
+        // }
     }
     var percentage=(parseInt(id)/6 * 100).toString()
     $(".progress").attr("style","height:30px")
